5befe2b03f7808a2faff119af4939dee89ca7f15
BMosnier <bastienmosnier@gmail.com>
2018-12-19 14:50:29 +0100

    gitignore + get

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+/*
 var http = require('http');
 
 var server = http.createServer(function(req, res){
     res.writeHead(200);
     res.end('hello world!');
 })
-server.listen(8080);
+server.listen(8080);*/
+
+import express from "express";
+import db from './db/db';
+const app = express();
+app.get('/api/v1/posts', (req, res) => {
+    res.status(200).send({
+        posts:db
+    })
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+});
