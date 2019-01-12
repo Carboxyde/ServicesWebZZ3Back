2ecbcd134738e0f6f0da72fe6ed7e2bf9ee7edee
BMosnier <bastienmosnier@gmail.com>
2019-01-12 17:03:51 +0100

    Résout le problème CORS

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ import posts from "./routes/controllers/posts.js"
 const app = express();
 const PORT = 5000;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use("/api/v1/posts", posts);
+
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
